26137251cb852cdfc6356019809c4fbaf7864488
Skaletto07 <Skaletto07@yandex.ru>
2022-06-21 21:42:44 +0300

    data jpa

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot_data_jpa
---
+++ src/main/java/com/kostkin/springboot/spring_data_jpa/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.kostkin.springboot.spring_data_jpa.service;
+
+
+import com.kostkin.springboot.spring_data_jpa.entity.Employee;
+
+import java.util.List;
+
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+
+    List<Employee> findAllByName(String name);
+}
